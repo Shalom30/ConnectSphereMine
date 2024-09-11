1d315b76cf7dc9c9999345bbe959d87f1d2ac8d7
Afa <banlaafa@gmail.com>
2024-09-11 09:40:29 +0100

    .

--- login.php
+++ login.php
@@ -37,7 +37,8 @@ session_start();
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title>My Book | Login page</title>
 	<style>
 		#bar{
